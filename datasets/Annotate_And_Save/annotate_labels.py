836d4a5e2fb4fe46776ed8bcaaf3468008016d57
cathmarcoux <marc1102@usherbrooke.ca>
2026-03-23 16:54:48 -0400

    Annotation des images 250-488 (18 mars)

--- datasets/Annotate_And_Save/annotate_labels.py
+++ datasets/Annotate_And_Save/annotate_labels.py
@@ -11,7 +11,11 @@ IMAGE_DIR = REPO_ROOT / "Tests_march_18/dataset/rgb"
 LABEL_DIR = REPO_ROOT / "Tests_march_18/dataset/labels"
 IMAGE_EXT = ".png"
 DATA_YAML = REPO_ROOT / "Tests_march_18/dataset/data.yaml"
+<<<<<<< Updated upstream
 START_INDEX = 230
+=======
+START_INDEX = 400
+>>>>>>> Stashed changes
 
 # ----------------------------------------
 
