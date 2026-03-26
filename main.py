eda769e673177c2cbb417090c867b7fc7bb0fc53
Rayvax2 <xlefebvre2003@gmail.com>
2026-03-26 13:54:39 -0400

    BUSH - Correction du Main

--- main.py
+++ main.py
@@ -18,10 +18,15 @@ NUM_IMAGES = 10
 MAX_FRAMES = 100
 PAUSE_KEY = ord('p')
 ESC_KEY = 27
-# ---------------------------------------
+PREDICT_MODE = True
+MODEL_PATH = SCRIPT_DIR / "datasets/Tests_Datasets_Roboflow/Data_1/runs/detect/train3/weights/best.pt"
+MODEL_PATH_IMG = SCRIPT_DIR / "datasets/Tests_Datasets_Roboflow/Data_1/valid/images"
 frames = []
+
+
 # ---------------- INPUT SOURCE ----------------
 if USE_CAMERA:
+
     pipeline = dai.Pipeline()
 
     monoLeft = pipeline.create(dai.node.Camera).build(dai.CameraBoardSocket.CAM_B)
@@ -63,6 +68,14 @@ if USE_CAMERA:
                 break
 # --------------------------------------------
 
+elif PREDICT_MODE:
+    model = YOLO(str(MODEL_PATH))
+    results = model.predict(
+        source=str(MODEL_PATH_IMG),
+        save=True,
+        conf=0.25
+    )
+
 else:
     print(f"Loading {NUM_IMAGES} images from {IMAGE_DIR}...")
 
@@ -79,83 +92,11 @@ else:
 # ---------------Example usage--------------
     for i, frame in enumerate(frames):
         img_float = frame.copy().astype(np.float32)
-        # Renormalization
-        # b, g, r = cv2.split(frame)
-        #
-        # b = cv2.normalize(b, None, 0, 255, cv2.NORM_MINMAX)
-        # g = cv2.normalize(g, None, 0, 255, cv2.NORM_MINMAX)
-        # r = cv2.normalize(r, None, 0, 255, cv2.NORM_MINMAX)
-        #
-        # enhanced = cv2.merge([b, g, r])
-        #
-        # enhanced = enhanced.astype(np.uint8)
-
-
-        # Weighted RG canals
-        # blue = img_float[:, :, 0]
-        # green = img_float[:, :, 1]
-        # red = img_float[:, :, 2]
-        #
-        # # Adjust each channel
-        # blue *= 0.6  # reduce blue dominance
-        # green *= 1.1  # slight boost
-        # red *= 1.8  # strong boost (compensate absorption)
-        #
-        # # Recombine
-        # enhanced = np.stack([blue, green, red], axis=2)
-        #
-        # # Clip to valid range
-        # enhanced = np.clip(enhanced, 0, 255).astype(np.uint8)
-
-
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # ---- PLOT ----
-        # plt.figure()  # smaller display
-        #
-        # plt.subplot(1, 2, 1)
-        # plt.imshow(frame_rgb)
-        # plt.title("Original")
-        # plt.axis('off')
-        #
-        # plt.subplot(1, 2, 2)
-        # plt.imshow(enhanced)
-        # plt.title("Enhanced (Weighted RG)")
-        # plt.axis('off')
-        #
-        # plt.tight_layout()
-        # plt.show()
-
-
-
-        edges = cv2.Canny(frame, 100, 200)
-        edges = cv2.dilate(edges, np.ones((3, 3), np.uint8), iterations=2)
-        edges = cv2.morphologyEx(edges, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
-
-        contours, _ = cv2.findContours(
-            edges,
-            cv2.RETR_EXTERNAL,
-            cv2.CHAIN_APPROX_SIMPLE
-        )
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(edges, (x, y), (x + w, y + h), (0, 255, 0), 1)
-
-        plt.figure()  # smaller display
-
-        plt.subplot(1, 2, 1)
-        plt.imshow(frame_rgb)
-        plt.title("Original")
-        plt.axis('off')
-
-        plt.subplot(1, 2, 2)
-        plt.imshow(edges)
-        plt.title("Edges")
-        plt.axis('off')
-
-        plt.tight_layout()
-        plt.show()
         print(f"Frame {i} shape: {frame.shape}")
 
-# ------------------------------------------
+
+# ---------------CALL FUNCTION--------------
+
+
+
 cv2.destroyAllWindows()
